90ed1dca8960834882c90920db545b395e05639b
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2024-08-29 17:36:04 +0200

    Updated typography

--- src/app/ui/typography.tsx
+++ src/app/ui/typography.tsx
@@ -2,6 +2,12 @@ export const textComponents = {
   heading2: ({ children }: {children: any}) => (
     <h2 className="font-sans font-bold text-black text-4xl pb-4 xl:[column-span:all]">{children}</h2>
   ),
+  heading3: ({ children }: {children: any}) => (
+    <h3 className="font-sans font-bold text-black text-2xl pb-4 xl:[column-span:all]">{children}</h3>
+  ),
+  heading4: ({ children }: {children: any}) => (
+    <h4 className="font-sans font-bold text-black text-lg pb-4 xl:[column-span:all]">{children}</h4>
+  ),
   paragraph: ({ children }: {children: any}) => (
     <p className="text-base font-normal pb-6 last:pb-0 max-w-prose">{children}</p>
   ),
